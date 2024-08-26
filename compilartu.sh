bca449c1c29bcc1f2b927005079d5803b12f46d4
Lartu <11744462+Lartu@users.noreply.github.com>
2024-08-25 22:08:59 -0300

    Refix compilartu

--- compilartu.sh
+++ compilartu.sh
@@ -1,11 +1,11 @@
 set -e
 
 # Compile the Katalyn Compiler (Python3)
-#python3.11 -m nuitka --standalone --onefile --deployment kat.py
-#rm -rf kat.build
-#rm -rf kat.dist
-#rm -rf kat.onefile-build
-#mv kat.bin ~/bin/kat
+python3.11 -m nuitka --standalone --onefile --deployment kat.py
+rm -rf kat.build
+rm -rf kat.dist
+rm -rf kat.onefile-build
+mv kat.bin ~/bin/kat
 
 # Compile the NariVM
 g++ -std=c++11 -I/opt/homebrew/opt/boost/include \
