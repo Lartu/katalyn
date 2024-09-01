f44d8b754aeaea2494fb887ecb4001a822773434
Lartu <11744462+Lartu@users.noreply.github.com>
2024-09-01 17:08:29 -0300

    Update compile_nvm.sh

--- compile_nvm.sh
+++ compile_nvm.sh
@@ -2,7 +2,7 @@ set -e
 
 # Compile the NariVM
 echo "Compiling NariVM"
-g++ -std=c++17 narivm.cpp -c -o narivm -O1
+g++ -std=c++17 narivm.cpp -c -o narivm.o -O1
 #g++ -std=c++11 -I/opt/homebrew/opt/boost/include -L/opt/homebrew/opt/boost/lib -lboost_system -lboost_filesystem narivm.o -o narivm -O3
 echo "Linking NariVM"
 g++ -std=c++17 narivm.o -o narivm -O1
