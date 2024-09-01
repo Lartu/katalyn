889e8982511071a4fc5682f43b3cba73e2f27a34
Lartu <11744462+Lartu@users.noreply.github.com>
2024-09-01 17:36:12 -0300

    Increase optimization

--- compile_nvm.sh
+++ compile_nvm.sh
@@ -1,14 +1,20 @@
 set -e
 
+optimization="-O3"
+
 # Compile the NariVM
 echo "Compiling NariVM"
-g++ -std=c++17 narivm.cpp -c -o narivm.o -O1
+g++ -std=c++17 narivm.cpp -c -o narivm.o $optimization
 
 echo "Building Tiny Process Library"
 processdir="tiny-process-library"
-g++ -std=c++17 "$processdir/process.cpp" -c -o process.o -O1
-g++ -std=c++17 "$processdir/process_unix.cpp" -c -o process_unix.o -O1
+g++ -std=c++17 "$processdir/process.cpp" -c -o process.o $optimization
+g++ -std=c++17 "$processdir/process_unix.cpp" -c -o process_unix.o $optimization
 
 echo "Linking NariVM"
-g++ -std=c++17 narivm.o process.o process_unix.o -o narivm -O1 -pthread
+g++ -std=c++17 narivm.o process.o process_unix.o -o narivm -pthread
+
+echo "Cleaning up"
+rm *.o
+
 mv narivm ~/bin/narivm
