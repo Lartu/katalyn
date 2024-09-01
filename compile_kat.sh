17a794b9052143d81591ef5fd7c47eb250ebc2f4
Lartu <11744462+Lartu@users.noreply.github.com>
2024-09-01 17:53:30 -0300

    Remove nuitka altogether

--- compile_kat.sh
+++ compile_kat.sh
@@ -1,9 +1,4 @@
 set -e
 
-# Compile the Katalyn Compiler (Python3)
-# If you don't have nuitka, install it via python3 -m pip install nuitka
-python3 -m nuitka --standalone --onefile --deployment kat.py
-rm -rf kat.build
-rm -rf kat.dist
-rm -rf kat.onefile-build
-mv kat.bin ~/bin/kat
+# Install the Katalyn compiler (Python3)
+mv kat.py ~/bin/kat
