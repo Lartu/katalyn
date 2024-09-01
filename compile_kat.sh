79bf46866d3b43dbafce52830eb13b2dc002585c
Lartu <11744462+Lartu@users.noreply.github.com>
2024-09-01 17:51:03 -0300

    Add nuitka note

--- compile_kat.sh
+++ compile_kat.sh
@@ -1,7 +1,7 @@
 set -e
 
 # Compile the Katalyn Compiler (Python3)
-python3 -m pip install nuitka --user --break-system-packages
+# If you don't have nuitka, install it via python3 -m pip install nuitka
 python3 -m nuitka --standalone --onefile --deployment kat.py
 rm -rf kat.build
 rm -rf kat.dist
