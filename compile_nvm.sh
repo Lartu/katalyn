878c9047be2687ea9ad7293b9d9ac46b26b1e353
Lartu <11744462+Lartu@users.noreply.github.com>
2024-09-01 17:34:31 -0300

    Add pthread to linker

--- compile_nvm.sh
+++ compile_nvm.sh
@@ -10,5 +10,5 @@ g++ -std=c++17 "$processdir/process.cpp" -c -o process.o -O1
 g++ -std=c++17 "$processdir/process_unix.cpp" -c -o process_unix.o -O1
 
 echo "Linking NariVM"
-g++ -std=c++17 narivm.o process.o process_unix.o -o narivm -O1
+g++ -std=c++17 narivm.o process.o process_unix.o -o narivm -O1 -pthread
 mv narivm ~/bin/narivm
